5b2a42b97c7ef4aba877517354e69c92764f9ef9
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-08-03 10:40:13 -0700

    first commit

---
+++ src/index/index.js
@@ -0,0 +1,7 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../init.scss' ;
+import './styles/landing.scss' ;
+//load scripts
+import './scripts/landing' ;
+
