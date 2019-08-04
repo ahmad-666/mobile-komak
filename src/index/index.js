2a9c93f1f1c9dc5334367a1d23c576823c59e96b
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-08-04 13:58:39 -0700

    add features

--- src/index/index.js
+++ src/index/index.js
@@ -3,6 +3,8 @@ import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
 import '../init.scss' ;
 import './styles/nav.scss' ;
 import './styles/landing.scss' ;
+import './styles/why-us.scss' ;
 //load scripts
 import './scripts/landing' ;
+import './scripts/why-us' ;
 
