1ad313fea9ffb735b8adb1400eb03527cdd0331c
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-08-13 14:04:34 -0700

    tabs animation

--- src/articles/articles.js
+++ src/articles/articles.js
@@ -4,6 +4,7 @@ import '../init.scss' ;
 import '../index/styles/nav.scss' ;
 import './styles/grid.scss' ;
 import './styles/tabs.scss' ;
+import './styles/articles.scss' ;
 import '../index/styles/slider.scss' ;
 import '../index/styles/footer.scss' ;
 import '../index/styles/copyright.scss' ;
@@ -11,6 +12,7 @@ import '../index/styles/copyright.scss' ;
 import '../index/scripts/landing' ;
 import './scripts/grid' ;
 import './scripts/tabs' ;
+import './scripts/articles' ;
 import '../index/scripts/slider' ;
 import '../index/scripts/footer' ;
 //load videos 
---
+++ src/articles/scripts/tabs.js
@@ -0,0 +1,38 @@
+let wrapper = document.querySelector('#tabs') ;
+let line = wrapper.querySelector('.line') ;
+let tabs = wrapper.querySelectorAll('ul li') ;
+let lastLi = tabs[tabs.length-1] ;
+let tabsContents = wrapper.querySelectorAll('.tabs_content > div') ;
+tabs.forEach(tab => {
+    tab.addEventListener('click',activateTab) ;
+});
+function getChildIndex(child){
+    let index = null ;
+    let parent = child.parentElement ;
+    let children = parent.children ;
+    children.forEach((c,i) => {
+        if(c==child)  index = i ;
+    })
+    return index ;
+}
+function getStyle(el,prop){
+    return window.getComputedStyle(el,null).getPropertyValue(prop) ;
+}
+function activateTab(e){
+    //activate tab
+    tabs.forEach(tab => {       
+        if(tab!=this) tab.classList.remove('active') ;
+        else tab.classList.add('active') ;
+    })
+    //move line
+    let lineOffset= getChildIndex(this)*parseFloat(getStyle(this,'width'))  ;
+    if(this==lastLi) line.style.width = `calc(20%)` ; 
+    else line.style.width = `calc(20% + 1.2px + .2px)` ;
+    line.style.right = `${lineOffset}px` ;
+    //appear tab content
+    let currTabContentID = this.getAttribute('data-target') ;
+    tabsContents.forEach(content => {
+        if(content.getAttribute('id') == currTabContentID) content.classList.add('active') ;
+        else content.classList.remove('active') ;
+    })
+}
