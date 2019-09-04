5ddeb9d58b2d825dcd701582558717da8bdf5346
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-09-04 15:04:42 -0700

    after second revision

--- src/article/scripts/article.js
+++ src/article/scripts/article.js
@@ -3,23 +3,23 @@ import util from '../../utilities/utilities' ;
 //--------------------------------------------
 //--------------------------------------------
 //--------------------------------------------
-let tabsWrapper = document.querySelector('main aside #tabs_popular') ;
-let tabsTriggers = tabsWrapper.querySelectorAll('li') ;
-let tabs = tabsWrapper.querySelectorAll('#tabs_content > div') ;
-tabsTriggers.forEach(tabTrigger => {
-    tabTrigger.addEventListener('click',activateTab) ;
-})
-function activateTab(e){
-    tabsTriggers.forEach(tabTrigger => {
-        if(this != tabTrigger) tabTrigger.classList.remove('active') ;
-        else this.classList.add('active') ;
-    })
-    let targetTabID = this.getAttribute('data-target') ;
-    tabs.forEach(tab => {
-        if(tab.getAttribute('id') != targetTabID) tab.classList.remove('active') ;
-        else tab.classList.add('active') ;
-    })
-}
+// let tabsWrapper = document.querySelector('main aside #tabs_popular') ;
+// let tabsTriggers = tabsWrapper.querySelectorAll('li') ;
+// let tabs = tabsWrapper.querySelectorAll('#tabs_content > div') ;
+// tabsTriggers.forEach(tabTrigger => {
+//     tabTrigger.addEventListener('click',activateTab) ;
+// })
+// function activateTab(e){
+//     tabsTriggers.forEach(tabTrigger => {
+//         if(this != tabTrigger) tabTrigger.classList.remove('active') ;
+//         else this.classList.add('active') ;
+//     })
+//     let targetTabID = this.getAttribute('data-target') ;
+//     tabs.forEach(tab => {
+//         if(tab.getAttribute('id') != targetTabID) tab.classList.remove('active') ;
+//         else tab.classList.add('active') ;
+//     })
+// }
 //ellipses
 //--------------------------------------------
 //--------------------------------------------
